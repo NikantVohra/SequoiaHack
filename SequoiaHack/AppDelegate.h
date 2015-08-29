7105b4e0d55f2754df0266c9e69a228cac193d8a
nvohra <nvohra@paypal.com>
2015-08-29 16:30:18 +0530

    Initial Commit

---
+++ SequoiaHack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SequoiaHack
+//
+//  Created by Vohra, Nikant on 29/08/15.
+//  Copyright © 2015 Vohra, Nikant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SequoiaHack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SequoiaHack
+//
+//  Created by Vohra, Nikant on 29/08/15.
+//  Copyright © 2015 Vohra, Nikant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
